9b1d85191e5ea8bbbd1b82bd3219702abcf04887
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-09 17:43:30 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get '/' do
+		"Hello, World!"
+	end
+	get '/name' do
+		self.data_points('name')
+	end
+	get '/hometown' do
+		self.data_points('hometown')
+	end
+	get '/favorite-song' do
+		self.data_points('favorite song')
+	end
+
+
+	def data_points(thing)
+		"My #{thing} is __"
+	end
 end
